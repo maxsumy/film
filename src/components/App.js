8c56f893bd38b2199824e51ff3aed0692720e0b6
unknown <grenki2@gmail.com>
2022-08-26 15:04:39 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import {Route, Switch} from 'react-router-dom';
+
+const App = () => {
+  return (
+    
+
+    <div>
+        <CssBaseline/>
+        <h2>Hello world Film Empire2</h2>
+        
+    </div>
+  )
+}
+
+export default App
